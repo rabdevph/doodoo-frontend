a62e832f16e2160c3238271ce53b657bc793c971
Ralph Arsy Bauto <rab.devph@gmail.com>
2023-12-22 10:05:44 +0400

    Add custom box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        common: '3px 6px 0px 0px rgba(0,0,0,1);',
+      },
+    },
   },
   plugins: [],
 };
