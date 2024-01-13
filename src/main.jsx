3e482a23fccfb9a004d00e9436c33ac0497c26b7
Ralph Arsy Bauto <rab.devph@gmail.com>
2024-01-13 10:18:46 +0400

    Add task context

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthContextProvider } from './context/AuthContext.jsx';
+import { TaskContextProvider } from './context/TaskContext.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <TaskContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </TaskContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 );
